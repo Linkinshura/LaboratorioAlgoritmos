24a90d78edffc52f2c035d07fabdcb8323ae079f
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-03-19 22:34:43 -0300

    Corregir a metodologia de trabajo Circunferencia2.c

--- Circunferencia2.c
+++ Circunferencia2.c
@@ -1,20 +1,18 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-int main (int argc, char *argv[]) {
+#include <stdio.h>
+#define pi 3.14
 
 float rad;
+float sup;
+
+main () {
+
 printf("Ingrese el radio:");
+
 scanf("%2f", &rad);
 
-float pi = 3.14;
-
-float sup = pi * (rad * rad);
-
-
+sup = pi * (rad * rad);
 printf("la Superficie es: %f \n el radio es: %f \n y el pi es: %f", sup, rad, pi);
 
-
-return 0;
 }
