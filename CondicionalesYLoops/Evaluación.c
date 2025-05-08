1f1423a25e6c8b880227224f9ecbb1de7633eb64
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-05-07 23:21:17 -0300

    Actualizar Evaluación.c

--- CondicionalesYLoops/Evaluación.c
+++ CondicionalesYLoops/Evaluación.c
@@ -1,1 +1,23 @@
-a
+#include <stdio.h>
+
+int main() {
+    int num, suma = 0;
+
+    printf("Ingrese un numero entero positivo (0 para terminar): ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        if(num > 0) {
+            suma += num;
+        } else {
+            printf("El numero no es positivo. Intente de nuevo.\n");
+        }
+
+        printf("Ingrese un numero entero positivo (0 para terminar): ");
+        scanf("%d", &num);
+    }
+
+    printf("La suma de los numeros positivos ingresados es: %d\n", suma);
+
+    return 0;
+}
