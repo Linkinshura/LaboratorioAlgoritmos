faa1537828e745639929f7801d6e379717ced076
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-05-07 23:24:30 -0300

    Actualizar Evaluación.c

--- CondicionalesYLoops/Evaluación.c
+++ CondicionalesYLoops/Evaluación.c
@@ -1,23 +1,34 @@
+/*
+Alumno: Ian Gutierrez
+Curso: 4to a�o 3era division turno noche Computacion
+Materia: Laboratorio de Algoritmos de Estructura de Datos
+Maquina: 6
+__________________________________________________________
+Ejercicio 1:
+Un banco le otorga interes a sus clientes del 3% mensual si se hace un deposito de mas de 3000. si se deposita menos tiene el 1% de interes.
+Informar, leyendo el monto que deposita el cliente, el total que retirara al finalizar el mes.
+
+*/
+
 #include <stdio.h>
+#define int1 1.03
+#define int2 1.01
 
-int main() {
-    int num, suma = 0;
 
-    printf("Ingrese un numero entero positivo (0 para terminar): ");
-    scanf("%d", &num);
+float depo, total;
 
-    while(num != 0) {
-        if(num > 0) {
-            suma += num;
-        } else {
-            printf("El numero no es positivo. Intente de nuevo.\n");
-        }
-
-        printf("Ingrese un numero entero positivo (0 para terminar): ");
-        scanf("%d", &num);
-    }
-
-    printf("La suma de los numeros positivos ingresados es: %d\n", suma);
-
-    return 0;
+main () {
+	
+	printf("Ingresar el monto a depositar:");
+	scanf("%f", &depo);
+	
+	if (depo < 3000) {
+	 	total = depo * int2;
+	 	printf("El monto que podra retirar a final del mes es: %f", total);
+	}
+	 else {
+	 	total = depo * int1;
+		printf("El monto que podra retirar a final del mes es: %f", total);
+	 }
+	 
 }
