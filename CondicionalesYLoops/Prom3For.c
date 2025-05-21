09930786eae7ff48eeb74e0adb470b25bd94528c
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-05-21 20:37:27 -0300

    Update Prom3For.c

--- CondicionalesYLoops/Prom3For.c
+++ CondicionalesYLoops/Prom3For.c
@@ -6,18 +6,23 @@ Realizar un programa que calcule el promedio de tres notas
 float nota, acum=0;
 float prom;
 float n = 3;
+int cont = 0;
 
 main () {
 		
 	int i = 0;
 	
 	for (i; i < n; i++) {
-		printf("Ingrese la nota del alumno %d:", i);
+		printf("Ingrese la nota del alumno %d:", i + 1);
 		scanf("%f", &nota);
+		
 		printf("\n");
-		acum += nota;
+		acum = acum + nota;
+		if (nota > 6) {
+			cont++;
+		}
 	}
 	prom = acum / n;
 	
-	printf("El promedio es %f", prom);
+	printf("El promedio es %f \n La cantidad de notas que hubo son: %f \n La cantidad aprobada es: %d", prom, n, cont);
 }
