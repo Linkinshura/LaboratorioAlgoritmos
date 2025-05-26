4b1e26f22ab276098b4b9a7e22104d8fb578e090
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-05-26 19:28:58 -0300

    Crear A.c

---
+++ CondicionalesYLoops/A.c
@@ -0,0 +1,39 @@
+/*
+Realizar un programa ingresan 5 pasajeros a un micro, se lee el destino:
+1 Pinamar
+2 Villa Gessel
+3 Mar del Plata 
+junto con la edad de cada pasajero.
+informar:
+1 porcentaje de mayores de edad
+2 Porcentaje que fue a Mar del Plata
+3 Promedio de edad de los que viajan a Gessel
+4 edad minima y su destino 
+*/
+
+#include <stdio.>
+
+float edad_Min, Prom_G;
+int i, n=5;ñ, edad_Min, edad, cant_MdP, destino;
+main(){
+
+for (i=0; i<n;i++){
+// Ingresar edad
+printf("Ingresar la edad:");
+scanf("%d", &edad);
+
+// Destinos
+printf("\n 1 Pinamar \n 2 Villa Gessel \n 3 Mar del Plata \n Eliga su destino);
+scanf("%d", &destino);
+
+// condiciones
+if (edad < 18){
+
+
+
+
+
+
+}
+
+
