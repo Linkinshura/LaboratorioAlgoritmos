610362e9d2db40264a0264eaf826b2f21247f636
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-06-18 15:55:46 -0300

    Update Vuela_Edad.c

--- CondicionalesYLoops/Vuela_Edad.c
+++ CondicionalesYLoops/Vuela_Edad.c
@@ -7,69 +7,57 @@ Realizar un programa que lea las edades de las personas que toman un vuelo y si 
 5- Expresar en porcentaje el item anterior
 */
 
-#include <stdio.h>
+int i=0, n=5, edad, tras, edad_min, tras_min;
+double porc_tras, prom_50_trasnt, porc_men_tras, cant_tras, cant_50, cant_50_trasnt, cant_men, cant_men_tras;
 
-int cant_tras, cant_Ntras_50, n=3, i=0, cant_m_edad_tr, edad, tras, edad_min=0, tras_min, cant_50, cant_m_edad;
+main (){
 
-float porc_tras, prom_ed_50_no, porc_men_tras, pre_porc_tras;
+   for(i; i<n;i++){
 
-main () {
-	/*
-	printf("Ingresar la cantidad de personas:");
-	scanf("%d", &n);
-	*/
-	for(i; i<n;i++){
+         printf("Ingresar la edad:");
+         scanf("%d", &edad);
 
-	printf("Ingresar la edad:");
-	scanf("%d", &edad);
-	
-	printf("¿Hace trasbordo?(1=si 0=no)");
-	scanf("%d", &tras);
-	
-	printf("\n");
-	
-	if (edad > 49){
-		cant_50++;	
-		
-	if (tras == 0){	
-			cant_Ntras_50++;
-	}
-}
-	if (tras == 1){
-		cant_tras++;
-	}
-	
-	if (edad <18){
-		cant_m_edad++;
-		if (tras == 1){
-			cant_m_edad_tr++;
-		}
-	}
-	
-	if (edad < edad_min && i != 0) {
-		edad_min = edad;
-		tras_min = tras;
-	}
-}
-	porc_tras = (cant_tras / n)  * 100.00;
-	
-	
-	prom_ed_50_no = cant_Ntras_50 / cant_50;
-	
-	
-	porc_men_tras = (cant_m_edad_tr / cant_m_edad)  * 100.00;	
+         printf("¿Hace trasbordo?(1=si 0=no)");
+         scanf("%d", &tras);
 
+         printf("\n");
 
- printf("El porcentaje de personas con trasbordo es: %f \n El promedio de mayores a 50 sin trasbordo es: %f \n El porcentaje de menores de edad con trasbordo es: %f \n El minimo de edad es : %d ", porc_tras, prom_ed_50_no, porc_men_tras, edad_min);
+     if (edad >= 50){
+              cant_50++;
+              if (tras == 0) {
+                       cant_50_trasnt++;
+                       }
+              }
+     if (edad <18) {
+              cant_men++;
+              if (tras == 1) {
+                       cant_men_tras++;
+                       }
+              }
 
- /*if (tras_min == 0) {
- 	printf("y no realiza trasbordo");
-printf("tras: %d \n", cant_tras); 
-printf("n= %d", n);
- } else if (tras_min == 1) {
- 	printf("y realiza trasbordo");
- }
-*/	
-	
-	
-}
+            if (edad_min > edad || i == 0){
+            edad_min = edad;
+            tras_min = tras;
+            }
+
+         if (tras == 1){
+                  cant_tras++;
+         }
+            
+     }
+
+     porc_tras = (cant_tras / n) * 100;
+
+     prom_50_trasnt = cant_50_trasnt / cant_50;
+
+     porc_men_tras = (cant_men_tras / cant_men) * 100;
+
+     printf("El porcentaje de trasbordo es: %2f \n El promedio de personas mayores a 50 sin trasbordo es: %2f \n El porcentaje de menores con trasbordo es: %2f \n La edad minima es: %d y ", porc_tras, prom_50_trasnt, porc_men_tras, edad_min);
+
+     if (tras_min == 1){
+                  printf("si realiza trasbordo");
+                  }
+     else if (tras_min == 0){
+          printf("no realiza trasbordo \n");
+          }
+     }
