4b1704c23b4aac4a49b8f5c141af96b91ab2cad6
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-09-03 22:27:14 -0300

    Update NotasCursos.c

--- CondicionalesYLoops/NotasCursos.c
+++ CondicionalesYLoops/NotasCursos.c
@@ -10,7 +10,7 @@ Rinden como maximo 5 estudiantes. Finaliza el programa al ingresar 0 como curso,
 
 int i=0, n=5;
 
-float edad_min, prom_n18, cant_nota, porc_18_1, porc_18_2, edad, curso, cant_n18, nota, acum_n18, nota_min, curso_min, cant_18_1, cant_18_2;
+float edad_min, prom_n18, cant_nota, porc_18_1, porc_18_2, edad, curso, cant_n18, nota, acum_n18, nota_min, curso_min, cant_18_1, cant_18_2, cant_2, cant_1;
 
 main(){
 	do{
@@ -44,16 +44,23 @@ main(){
 				cant_18_2++;
 			}
 		}
+if (curso == 1){
+	cant_1++;
+}
+		else if (curso == 2){
+			cant_2++;
+		}
 	
 		i++;
-	}while(curso != 0);
+	}while(curso != 0 && i<n);
 	
 	porc_18_1 = (cant_18_1 / n) * 100;
 	porc_18_2 = (cant_18_2 / n) * 100;
 	prom_n18 = acum_n18 / cant_n18;
 	
-	printf("\n 1. El estudiante con menor nota tiene %f años \n y pertenece al curso numero %f \n", edad_min, curso_min);
+	printf("\n 1. El estudiante con menor nota tiene %f aÃ±os \n y pertenece al curso numero %f \n", edad_min, curso_min);
 	printf("2. El porcentaje de alumnos mayores de edad por curso es: \n %f porciento para el curso 1 \n %f porciento para el curso 2 \n", porc_18_1, porc_18_2);
 	printf("3. La nota promedio entre menores de edad es: %f", prom_n18);
 	
 }
+
