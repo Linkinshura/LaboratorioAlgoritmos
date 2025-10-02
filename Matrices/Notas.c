c2bc29c87dfc2deed5fb203ad587331afde9a17e
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-10-01 22:21:34 -0300

    Update Notas.c

--- Matrices/Notas.c
+++ Matrices/Notas.c
@@ -9,15 +9,15 @@ Mostrar:
 
 #include <stdio.h>
 
-int notas[3][3], cant_e, acum_e, cant_p, acum_p, nota_min, i, j, n=3, pos_min_i, pos_min_j, diagonal[3][3];
+int notas[3][3], acum_e, acum_p, nota_min, i, j, n=3, pos_min_i, pos_min_j, diagonal[3][3];
 float prom_e, prom_p;
 
 main(){
 	for(i=0; i<n; i++){
 		// Estudiante
-		cant_e=0;
+	
 		acum_e=0;
-		cant_p=0;
+		
 		acum_p=0;
 		for(j=0; j<n; j++){
 			// Prueba
@@ -25,9 +25,9 @@ main(){
 			scanf("%d", &notas[i][j]);
 			
 			printf("\n");
-			cant_e++;
+			
 			acum_e+= notas[i][j];
-			prom_e= acum_e/cant_e;
+			prom_e= acum_e/n;
 
 				if (notas[i][j] < nota_min || (i == 0 && j == 0)){
 					nota_min= notas[i][j];
@@ -59,14 +59,14 @@ main(){
 		}	
 			for(j=0; j<n; j++){
 		printf("\n");
-		cant_p=0;
+
 		acum_p=0;
 		prom_p=0;
 		for(i=0; i<n; i++){
-				cant_p++;
 				acum_p+= notas[i][j];
-				prom_p= acum_p/cant_p;
+			
 				}
+				prom_p= acum_p/n;
 			printf("Promedio de la prueba %d:%f", j+1, prom_p);
 		}	
 			
