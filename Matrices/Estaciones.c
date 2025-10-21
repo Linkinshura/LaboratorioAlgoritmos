f2983d9b00a8621ed6d139bb992ce662825bcf79
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-10-21 20:51:32 -0300

    Add files via upload

--- Matrices/Estaciones.c
+++ Matrices/Estaciones.c
@@ -1,102 +1,85 @@
 /*
-Se registran las personas que usan el subte en tres estaciones a lo largo de tres dias.
-Mostrar:
-1- Promedio de personas por estacion y en que estacion hubieron mas personas(en funcion del promedio)
-2- Promedio de pasajeros por dia y cuantos dias usaron el servicio mas de 300 pasajeros. Porcentaje
-3- Minimo valor de la matriz, posicion de memoria y diagonal principal
-4- Por cada estacion mostrar que dia fue mas concurrido
+Una aerolinea registra las personas que viajan a 3 destinos durante 3 dias, mostrar:
+1- Total por destino y que destino tuvo menos visitantes
+2- Promedio por dia y cuantos dias volaron mas de 500 personas. Expresar en porcentaje
+3- Maximo valor, posiciones y traza
+4- En cada dia mostrar el destino menos transcurrido
 
-i = dia
-j = estacion
+i=Destino
+j=Dia
 */
+
+
 #include <stdio.h>
-
-
-int i, j, n=3, estaciones[3][3], diagonal[3][3], cant_est, cant_dia, est_min, i_min, j_min, est_max, cant_300, pos_min_i, pos_min_j, i_max;
-float prom_est, prom_dia, porc_300;
-
-main (){
-	for(i=0;i<n;i++){
-		cant_est = 0;
-		for(j=0;j<n;j++){
-			printf("Ingrese la cantidad de pasajeros en el dia %d en la estacion %d:", j+1, i+1);
-			scanf("%d", &estaciones[i][j]);
-			printf("\n");
-			cant_est+=estaciones[i][j];
+int pasa[3][3], i, j, N=3, acum=0, max, traza[3][3], pos_i, pos_j, min, dia_min;
+float prom;
+main(){
+	//CARGA
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			printf("Ingrese pasajeros al destino %d en el dia %d:", i,j);
+			scanf("%d", &pasa[i][j]);
 		}
-		prom_est =cant_est / n;
-		printf("El promedio de la estacion %d es: %f", i+1, prom_est);
-		printf("\n");
+	}
+	// 1.personas por destino (filas)
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			acum=acum+pasa[i][j];
+		}
+		printf("Total de personas del destino %d es %d \n", i, acum);
+		acum=0;
+		
+	}
+
+    // 2.promedio por dia (columnas)
+	for(j=0; j<N; j++){
+		acum_dia=0;
+		for(i=0; i<N; i++){
+			acum=acum+pasa[i][j];
+
+		}
+		prom=acum/N;
+		printf("El promedio de personas del dia %d es %f \n", j, prom);
+		acum=0;
+		
 	}
 	
-	for(i=0;i<n;i++){
-		printf("\n |");
-		for(j=0;j<n;j++){
-			printf("%d ", estaciones[i][j]);
-		}
-		printf("|");
-	}
-	
-	for(j=0;j<n;j++){
-		cant_dia=0;
-		cant_300=0;
-		for(i=0;i<n;i++){
-			cant_dia+=estaciones[i][j];
-			if (cant_dia >=300){
-				cant_300++;
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			if( (i==0 && j==0)|| pasa[i][j]>max ){
+				max=pasa[i][j];
+				pos_i=i;
+				pos_j=j;
 			}
-		}
-		prom_dia=cant_dia/n;
-		printf("\n");
-		printf("El promedio del dia %d es: %f", j+1, prom_dia);
-		printf("\n");
-		printf("Fueron %d viajes con 300 o mas pasajeros", cant_300);
-		printf("\n");
-	}
-	
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-			if (i == j){
-				diagonal[i][j] = estaciones[i][j];
+			if(i==j){
+				traza[i][j] = pasa[i][j];
 			} else {
-				diagonal[i][j] = 0;
+				traza[i][j] = 0;
 			}
-			if (estaciones[i][j] < est_min || (i == 0 && j == 0)){
-					est_min= estaciones[i][j];
-					pos_min_i= i + 1;
-					pos_min_j = j + 1;
-				}
-		}
+		}		
 	}
+	printf("El maximo es %d y se encuenta en i= %d j= %d \n", max, pos_i, pos_j);
 	
-	
-	printf(" El valor minimo es: %d y fue en el dia:%d y la estacion: %d", est_min, pos_min_i, pos_min_j);
-	printf("\n");
-	
-	porc_300=(cant_300/n) * 100;
-	
-	printf("El %f porciento de los viajes tuvieron 300 o mas pasajeros", porc_300);
-	printf("\n");
-	
-	printf("Diagonal Principal: \n");
-	for(i=0;i<n;i++){
+	printf("La traza es: \n");
+	for(i=0;i<N;i++){
 		printf("|");
-		for(j=0;j<n;j++){
-			printf("%d ", diagonal[i][j]);
+		for(j=0;j<N;j++){
+			printf(" %d", traza[i][j]);
 		}
 		printf("|");
 		printf("\n");
 	}
 	
-	for(j=0;j<n;j++){
-		for(i=0;i<n;i++){
-			if (estaciones[i][j] > est_max || (i == 0 && j == 0)){
-					est_max= estaciones[i][j];
-					i_max=i+1;
-				}
+
+
+	for(j=0;j<N;j++){
+		for(i=0;i<N;i++){
+			if (min> pasa[i][j] || i == 0) {
+				min=pasa[i][j];
+				pos_i_min= i;
+				pos_j_min= j;
+			}
 		}
-		printf("\n");
-		printf("El dia mas concurrido de la estacion %d fue el dia %d y hubo %d pasajeros", j+1, i_max, est_max);
-		printf("\n");
 	}
+	
 }
